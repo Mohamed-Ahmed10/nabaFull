7bea6fe6a1f7865dffa1b4607510bc4a355975d1
amrhuusien99 <amrhuusien99@gmail.com>
2023-02-20 11:04:38 +0200

    a

--- app/Http/Middleware/CheckDataForHeader.php
+++ app/Http/Middleware/CheckDataForHeader.php
@@ -23,30 +23,31 @@ class CheckDataForHeader
      */
     public function handle(Request $request, Closure $next)
     {
-        if(Session::has('nav_products') && Session::has('nav_articles') && Session::has('nav_webinars') && Session::has('nav_services') && Session::has('nav_trainings')) {
-            return $next($request);
-        }else{
-            if(Session::has('nav_products')) {
-                $products = Product::translatedIn(LaravelLocalization::setLocale())->active()->get();
-                Session::put("nav_products", $products, now()->addMonth(1));
-            }
-            if(Session::has('nav_articles')) {
-                $articles = Article::translatedIn(LaravelLocalization::setLocale())->active()->get();
-                Session::put("nav_articles", $articles, now()->addMonth(1));
-            }
-            if(Session::has('nav_webinars')) {
-                $webinars = Webinar::translatedIn(LaravelLocalization::setLocale())->active()->get();
-                Session::put("nav_webinars", $webinars, now()->addMonth(1));
-            }
-            if(Session::has('nav_services')) {
-                $services = Service::translatedIn(LaravelLocalization::setLocale())->active()->get();
-                Session::put("nav_services", $services, now()->addMonth(1));
-            }
-            if(Session::has('nav_trainings')) {
-                $trainings = Training::translatedIn(LaravelLocalization::setLocale())->active()->get();
-                Session::put("nav_trainings", $trainings, now()->addMonth(1));
-            }
-            return $next($request);
-        }
+        return $next($request);
+        // if(Session::has('nav_products') && Session::has('nav_articles') && Session::has('nav_webinars') && Session::has('nav_services') && Session::has('nav_trainings')) {
+        //     return $next($request);
+        // }else{
+        //     if(Session::has('nav_products')) {
+        //         $products = Product::translatedIn(LaravelLocalization::setLocale())->active()->get();
+        //         Session::put("nav_products", $products, now()->addMonth(1));
+        //     }
+        //     if(Session::has('nav_articles')) {
+        //         $articles = Article::translatedIn(LaravelLocalization::setLocale())->active()->get();
+        //         Session::put("nav_articles", $articles, now()->addMonth(1));
+        //     }
+        //     if(Session::has('nav_webinars')) {
+        //         $webinars = Webinar::translatedIn(LaravelLocalization::setLocale())->active()->get();
+        //         Session::put("nav_webinars", $webinars, now()->addMonth(1));
+        //     }
+        //     if(Session::has('nav_services')) {
+        //         $services = Service::translatedIn(LaravelLocalization::setLocale())->active()->get();
+        //         Session::put("nav_services", $services, now()->addMonth(1));
+        //     }
+        //     if(Session::has('nav_trainings')) {
+        //         $trainings = Training::translatedIn(LaravelLocalization::setLocale())->active()->get();
+        //         Session::put("nav_trainings", $trainings, now()->addMonth(1));
+        //     }
+        //     return $next($request);
+        // }
     }
 }
