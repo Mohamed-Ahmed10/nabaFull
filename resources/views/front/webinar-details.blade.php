fabf7198be081369da8da2673ab669b17c0aaea0
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-03-13 00:50:46 +0200

    test

--- resources/views/front/webinar-details.blade.php
+++ resources/views/front/webinar-details.blade.php
@@ -121,11 +121,11 @@
 								@error('notes')
 									<span class="text-danger">{{$message}}</span>
 								@enderror
-							</div> 
+							</div>
 							<div class="m-3">
 								{!! NoCaptcha::renderJs() !!}
 								{!! NoCaptcha::display() !!}
-							</div> 
+							</div>
 							<div class="m-3">
 								<input type="submit" value="Submit">
 							</div>
@@ -137,76 +137,6 @@
 	</div>
 	<!-- end contact form -->
 
-	<div class="footer-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-4 col-md-12">
-					<div class="footer-box about-widget">
-						<h2 class="widget-title">About us</h2>
-						<p>Naba Soft was established in 2000 as a specialist in all fields of computer technologies. It
-							provides technical support, research, information, hardware, systematic and network support
-							to all governmental and private sector companies and establishments. We have developed the
-							experience that we acquired throughout many years</p>
-					</div>
-				</div>
-				<div class="col-lg-8 col-md-12">
-					<div class="footer-box pages get-in-touch">
-						<h2 class="widget-title">Contact Us</h2>
-						<ul>
-							<ul class="row">
-								<li class="col-lg-4 col-sm-12">
-									<h6>Dubai Office</h6>
-									<div>
-										<a href="tel:971552243426">+971-552243426</a>
-										<i class="fas fa-phone-volume m-1"></i>
-										<a href="https://wa.me/971552243426" target="_blank"><i
-												class="fab fa-whatsapp m-1"></i></a>
-									</div>
-								</li>
-								<li class="col-lg-4 col-sm-12">
-									<h6>Riyadh Office</h6>
-									<div>
-										<a href="tel:966570005134">+966-570005134</a>
-										<i class="fas fa-phone-volume m-1"></i>
-										<a href="https://wa.me/966570005134" target="_blank"><i
-												class="fab fa-whatsapp m-1"></i></a>
-									</div>
-								</li>
-								<li class="col-lg-4 col-sm-12">
-									<h6>R.A.K Office</h6>
-									<div>
-										<a href="tel:971589618351">+971-589618351</a>
-										<i class="fas fa-phone-volume m-1"></i>
-										<a href="https://wa.me/971589618351" target="_blank"><i
-												class="fab fa-whatsapp m-1"></i></a>
-									</div>
-								</li>
-								<li class="col-lg-4 col-sm-12">
-									<h6>Muscat Office</h6>
-									<div>
-										<a href="tel:96891113014">+968-91113014</a>
-										<i class="fas fa-phone-volume m-1"></i>
-										<a href="https://wa.me/96891113014" target="_blank"><i
-												class="fab fa-whatsapp m-1"></i></a>
-									</div>
-								</li>
-								<li class="col-lg-4 col-sm-12">
-									<h6>Cairo Office</h6>
-									<div>
-										<a href="tel:201020111351">+20-1020111351</a>
-										<i class="fas fa-phone-volume m-1"></i>
-										<a href="https://wa.me/01020111351" target="_blank"><i
-												class="fab fa-whatsapp m-1"></i></a>
-									</div>
-								</li>
-							</ul>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- end footer -->
 
 @endsection
 <!-- custom js -->
